1c6b44332f92e3f5fad52118679638a90efb6818
felipe <felipe.peixoto@msn.com>
2020-06-02 11:51:52 -0300

    Exercicio 1 Imprimir em ordem decrescente

---
+++ src/main/Exercicio1.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Exercicio1 {
+    public static void main(String [] args){
+        for(int i= 100; i>=1; i--){
+                System.out.println(i);
+        }
+    }
+}
