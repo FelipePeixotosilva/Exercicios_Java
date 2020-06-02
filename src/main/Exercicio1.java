9e893bd97cbe01ae364a52c72f8983630e2e16d0
felipe <felipe.peixoto@msn.com>
2020-06-02 10:42:09 -0300

    Escreva um programa que exiba os números de 1 a 100 na tela em ordem decrescente.

---
+++ src/main/Exercicio1.java
@@ -0,0 +1,16 @@
+package main;
+/*******************************************************************************
+ * 1) Escreva um programa que exiba os números de 1 a 100 na tela em ordem
+ * decrescente.
+ *******************************************************************************/
+public class Exercicio1 {
+
+    public static void main(String [] args) {
+        for (int i = 100; i >= 1; i--) {
+
+            System.out.println(i);
+        }
+
+    }
+}
+
