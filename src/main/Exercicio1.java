bfda718c23f57e1b1690b86add40bd53e6a2deb9
felipe <felipe.peixoto@msn.com>
2020-06-02 11:39:21 -0300

    Exercicio 1 Ordem decrescente

---
+++ src/main/Exercicio1.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Exercicio1 {
+    public static void main(String [] args){
+
+
+        for(int i=100; i>=1; i--){
+
+            System.out.println(i);
+        }
+    }
+}
